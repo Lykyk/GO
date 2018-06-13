faa234f7cd7275f270d4da33b5b49ef3a021b949
Lykyk <787202867@qq.com>
2018-06-14 01:33:59 +0800

    绑定学号（不检查学号）

--- http.go
+++ http.go
@@ -2,58 +2,59 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"log"
+	"net/http"
 	"strings"
 
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 
-	"github.com/silenceper/wechat"	//go get github.com/silenceper/wechat 执行后才能运行
+	"github.com/silenceper/wechat" //go get github.com/silenceper/wechat 执行后才能运行
 	"github.com/silenceper/wechat/message"
 )
 
-func GetOne(rows *sql.Rows) []string {
-    if rows == nil {
-        return nil
-    }
-    
-    cols, err := rows.Columns()
-    
-    rawResult := make([][]byte, len(cols))
-    result := make([]string, len(cols))
-    dest := make([]interface{}, len(cols))
-    for i, _ := range rawResult {
-        dest[i] = &rawResult[i]
-    }
-    
-    if rows.Next() {
-        err = rows.Scan(dest...)
-        
-        for i, raw := range rawResult {
-            if raw == nil {
-                result[i] = ""
-            } else {
-                result[i] = string(raw)
-            }
-        }
-        
-        //fmt.Printf("%#v\n", result)
-        
-        //break
-    } else {
-        return nil
-    }
-    
-    _=err
-    return result
-}
+//以 []string 形式返回一行数据，并将指针移到下一行
+// func GetOne(rows *sql.Rows) []string {
+// 	if rows == nil {
+// 		return nil
+// 	}
+
+// 	cols, err := rows.Columns()
+
+// 	rawResult := make([][]byte, len(cols))
+// 	result := make([]string, len(cols))
+// 	dest := make([]interface{}, len(cols))
+// 	for i, _ := range rawResult {
+// 		dest[i] = &rawResult[i]
+// 	}
+
+// 	if rows.Next() {
+// 		err = rows.Scan(dest...)
+
+// 		for i, raw := range rawResult {
+// 			if raw == nil {
+// 				result[i] = ""
+// 			} else {
+// 				result[i] = string(raw)
+// 			}
+// 		}
+
+// 		//fmt.Printf("%#v\n", result)
+
+// 		//break
+// 	} else {
+// 		return nil
+// 	}
+
+// 	_ = err
+// 	return result
+// }
 
 func hello(rw http.ResponseWriter, req *http.Request) {
 
 	//配置微信参数
 	config := &wechat.Config{
-		AppID:          "wx8781b9f815a3ca4c",
+		AppID: "wx8781b9f815a3ca4c",
 		// AppSecret:      "your app secret",
 		Token:          "baixuewuyaak47",
 		EncodingAESKey: "YOqOkJTgYeEtPHC77hRpCiE3qiLUa4cjskst60QHlsu",
@@ -73,7 +74,8 @@ func hello(rw http.ResponseWriter, req *http.Request) {
 		var split_message []string
 		var return_message string
 
-	 	
+		// log.Println("openid =", msg.FromUserName)		//打印日志信息
+		log.Println("用户信息<-：" + msg.Content) //打印日志信息:用户发送的信息
 
 		dbtemp, err := sql.Open("mysql", "root:baixuewuyaak47@/wx?charset=utf8")
 		db := dbtemp
@@ -89,21 +91,38 @@ func hello(rw http.ResponseWriter, req *http.Request) {
 		// 	log.Println(row[0] + " " + row[1])	//输出
 		// }
 
-		fmt.Println("openid =", msg.FromUserName)
-		rows, err := db.Query("select * from student")
-		fmt.Println(err)
-		_ = rows
-		_ = err
+		// rows, err := db.Query("select * from student")
+		// fmt.Println(err)
+		// _ = rows
+		// _ = err
 
 		// msg_content = string(msg.Content)
-		split_message = strings.Split(msg.Content, " ")	//以空格为标志分解用户信息
-		return_message = "openid = " + msg.FromUserName + "\n动作标志：" + split_message[0] + "\n附带信息：" + split_message[1]	//生成回复信息
-		
-		text := message.NewText(return_message)
-		log.Println("用户信息<-：" + msg.Content)		//打印日志信息
-		log.Println("返回信息->：" + return_message)	//打印日志信息
+		split_message = strings.Split(msg.Content, " ") //以空格为标志分解用户信息
+		//生成回复信息
+		return_message = "openid = " + msg.FromUserName + "\n动作标志：" + split_message[0] + "\n附带信息：" + split_message[1]
+
+		if split_message[0] == "BDXH" {
+			id := split_message[1]     //学号
+			openid := msg.FromUserName //openid
+
+			sql := "INSERT INTO `wx`.`user` (`id`, `openid`) VALUES ('" + id + "', '" + openid + "');"
+			rows, err := db.Query(sql)
+			_ = rows
+			_ = err
+			// if err != nil {
+			// 	fmt.Println("db.Query(sql) 执行发生错误:", err.Error())
+			// }
+
+			if err == nil {
+				return_message += "\n\n绑定学号成功。"
+			}else if strings.Contains(err.Error(), "Error 1062: Duplicate entry") {
+				return_message += "\n\n此学号已经绑定过"
+			}
+		}
 
 		// db.Close()
+		log.Println("回复信息->：" + return_message) //打印日志信息:回复给用户的信息
+		text := message.NewText(return_message)
 		return &message.Reply{MsgType: message.MsgTypeText, MsgData: text}
 	})
 
